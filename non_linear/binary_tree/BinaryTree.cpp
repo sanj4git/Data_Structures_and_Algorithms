80a3bde905ee88a167b48fc5a732d7afa9a8f37d
mdxaasil <88720914+mdxaasil@users.noreply.github.com>
2023-07-02 19:07:41 +0530

    Add files via upload

---
+++ non_linear/binary_tree/BinaryTree.cpp
@@ -0,0 +1,132 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MAX_SIZE 25000
+
+typedef struct Node {
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int value) : data(value), left(nullptr), right(nullptr) {}
+} Node;
+
+class BinaryTree {
+
+	Node* inserter(Node* root, int key) {
+		Node* newnode = new Node(key);
+
+		if (root == nullptr)
+		{
+			root = newnode;
+			return root;
+		}
+		else {
+
+			Node* q[MAX_SIZE];
+			int f, r;
+			f = r = -1;
+
+
+			q[++r] = root;
+			f = r = 0;
+
+			while (f <= r) {
+				Node* temp = q[f];
+				f++;
+
+				if (temp->left != NULL)
+					q[++r] = temp->left;
+
+				else {
+					temp->left = newnode;
+					return root;
+				}
+
+				if (temp->right != NULL)
+					q[++r] = temp->right;
+				else {
+					temp->right = newnode;
+					return root;
+				}
+
+			}
+
+
+		}
+
+	}
+
+public:
+	Node* root;
+	BinaryTree() {
+		root = nullptr;
+	}
+
+	insert(int key) {
+
+		root = inserter(root, key);
+
+	}
+
+	void inorderTraversal(Node* root) {
+		if (root == NULL)
+			return ;
+
+		inorderTraversal(root->left);
+		cout << root->data << " ";
+		inorderTraversal(root->right);
+
+	}
+
+	void mirror(Node* node) {
+
+		if (node->right)
+			mirror(node->right);
+		if (node->left)
+			mirror(node->left);
+
+		swap(node->left, node->right);
+
+	}
+
+	void leafProduct(Node* root, int &product) {
+		if (root->left == NULL && root->right == NULL)
+			product *= root->data;
+
+		else {
+			leafProduct(root->left, product);
+			leafProduct(root->right, product);
+		}
+	}
+
+	void leafSum(Node* root, int &summ) {
+		if (root->left == NULL && root->right == NULL)
+			summ += (root->data);
+
+		else {
+			leafSum(root->left, summ);
+			leafSum(root->right, summ);
+		}
+	}
+};
+
+int main() {
+	BinaryTree t;
+
+	t.insert(1);
+	t.insert(2);
+	t.insert(3);
+
+
+	int product = 1, summ = 0;
+	t.leafProduct(t.root, product);
+	cout << product;
+
+	t.leafSum(t.root, summ);
+	cout << summ;
+
+	// t.inorderTraversal(t.root);
+
+	return 0;
+}
---
+++ non_linear/binary_tree/ExpressionTree.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct node {
+	char data;
+	struct node*left, *right;
+
+	node(char val) : data(val), right(nullptr), left(nullptr) {}
+} node;
+
+node* build(string& s)
+{
+	int priority[128] = {0};
+	priority['+'] = priority['-'] = 1;
+	priority['*'] = priority['/'] = 2;
+	priority['^'] = 3;
+	priority[')'] = 0;
+
+	stack <char> charStack; //for storing symbols and brackets
+	stack<node*> nodeStack; //for storing node pointers
+
+	node* t, * t1, *t2; // t->parent, t1,t2->children
+
+	for (int i = 0; i < s.size(); i++) { //iterate through infixstring
+
+		if (s[i] == '(')
+			charStack.push(s[i]);
+
+		else if (isalpha(s[i]))
+		{
+			t = new node(s[i]);
+			nodeStack.push(t);
+
+		}
+
+		else if (priority[s[i]] > 0) {
+
+			while (
+			    (!charStack.empty() && charStack.top() != '(')
+			    &&
+			    (
+			        (s[i] != '^' && priority[charStack.top()] >= priority[s[i]])
+// ||
+// (s[i] == '^' && priority[charStack.top()] > priority[s[i]] )// to evaluate from right
+			    )
+
+			) {
+				t = new node(charStack.top());
+				charStack.pop();
+
+				t1 = nodeStack.top(); nodeStack.pop(); //operand 2
+				t2 = nodeStack.top(); nodeStack.pop(); //operand 1
+
+				t->left = t2;  //     op     |    t
+				t->right = t1; // op1   op2  | t2    t1
+
+				nodeStack.push(t);
+			}
+
+			charStack.push(s[i]);
+		}
+		else if (s[i] == ')')
+		{
+			while (!charStack.empty() && charStack.top() != '(') {
+				t = new node(charStack.top());
+				charStack.pop();
+
+				t1 = nodeStack.top(); nodeStack.pop();
+				t2 = nodeStack.top(); nodeStack.pop();
+
+				t->left = t2;
+				t->right = t1;
+
+				nodeStack.push(t);
+			}
+			charStack.pop();// popping the '('
+		}
+	}
+
+	t = nodeStack.top(); //root node
+
+	return t;
+}
+
+void postOrder(node* root) {
+	if (root == NULL) return ;
+	postOrder(root->left);
+	postOrder(root->right);
+	cout << root->data;
+}
+
+int main() {
+	string s; cin >> s;
+
+	s = "(" + s + ")";
+
+	node*root = build(s);
+
+	postOrder(root);
+	return 0;
+}
