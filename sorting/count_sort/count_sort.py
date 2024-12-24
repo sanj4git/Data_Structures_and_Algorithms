af27a6335f05b08931c5305f26ffd5e97c969299
Leela0o5 <156287943+Leela0o5@users.noreply.github.com>
2024-12-24 14:57:41 +0530

    Update count_sort.py

--- sorting/count_sort/count_sort.py
+++ sorting/count_sort/count_sort.py
@@ -5,32 +5,39 @@ https://www.ripublication.com/ijaer18/ijaerv13n1_28.pdf
 def counting_sort(L):
     if not L:
         return L
+        
     largest = max(L)
     min_value = min(L)
-    offset = -min_value if min_value<0 else 0
+    offset = -min_value if min_value < 0 else 0
     n = len(L)
+    
+    # Initialize the frequency array.
     frequencyArray = [0]*(largest+1+offset)
+
+    # Count the frequency of each element in the array.
     for i in range(n):
-     # shift of the element  by the offset which ensures negative numbers are shifted to non-negative indices in frequencyArray
+        # Shift the element by the offset which ensures negative numbers are shifted to non-negative indices in the frequencyArray.
         frequencyArray[L[i]+offset] = frequencyArray[L[i]+offset]+1
-    #sorting part
+        
+    # Sorting part.
     j = 0
     for i in range(len(frequencyArray)):
         while(frequencyArray[i]>0):
-            L[j] = i - offset #restores our original numbers after sorting
+            # Restores our original numbers after sorting.
+            L[j] = i - offset 
             frequencyArray[i] = frequencyArray[i] - 1
             j+=1
+            
     return L
 
+
+# Test cases
 L = [1, 4, 1, 3, 2, 4, 3, 7]
-print(counting_sort(L))
-#output: [1, 1, 2, 3, 3, 4, 4, 7]
+print(counting_sort(L)) # Output: [1, 1, 2, 3, 3, 4, 4, 7]
 
 L2 = [5,-4,-3,-2,4,7,9]
-print(counting_sort(L2))
-#output:[-4, -3, -2, 4, 5, 7, 9]
+print(counting_sort(L2)) # Output: [-4, -3, -2, 4, 5, 7, 9]
 
 L3 = []
-print(counting_sort(L3))
-#output:[]
+print(counting_sort(L3)) # Output: []
 
